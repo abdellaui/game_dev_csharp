af8cb8ee7f28bfff7130436c654331dc776641ff
=Abdullah Sahin <abdullah@sahin.uk>
2019-03-06 12:21:28 +0100

    test

---
+++ test_game/TestGame/Assets/Player/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] public float speed = 10f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Debug.Log(movement);
+        rb.AddForce(movement * speed);
+    }
+}
